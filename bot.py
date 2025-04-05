c9ad588fb6ea0b553be81308ef1fdc8a8b832187
MishaBoiko <misa85819@gmail.com>
2025-04-05 19:56:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,39 @@
 import json
-from aiogram import Bot, Dispatcher, types, executor
-from aiogram.dispatcher import FSMContext
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.fsm.state import State, StatesGroup
-from config import TOKEN
+import asyncio
 from pathlib import Path
 from datetime import datetime
-import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN,parse_mode='HTML')
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+db_file = Path('db.json')
+
+def read_db():
+    if not db_file.exists():
+        return {}
+    with db_file.open('r',encoding='utf-8') as f:
+        return json.load(f)
+
+def write_db(data):
+    with db_file.open('w',encoding='utf-8') as f:
+        return json.dump(data,f,indent=2, ensure_ascii=False)
+
+main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+main_keyboard.add(KeyboardButton("/new"), KeyboardButton("/notes"))
+
+class NoteForm(StatesGroup):
+    title = State()
+    description = State()
+    remind_at = State()
+    
+
